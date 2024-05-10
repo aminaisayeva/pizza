5ac26d9fff410ac5376f0234d2d4bdd89cbf7c02
ai2464 <ai2464@columbia.edu>
2024-05-09 20:17:44 -0400

    at last, added the chat-bot icon

--- columbia-puzzle/src/components/ChatBotIcon.js
+++ columbia-puzzle/src/components/ChatBotIcon.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../styles/ChatBotIcon.css';
+import chatBotIcon from '../assets/chat_bot_icon.png';
+
 
 const ChatBotIcon = ({ onClick }) => {
   return (
-    <div className="chatbot-icon" onClick={onClick}></div>
+    <img className="chatbot-icon" src={chatBotIcon} alt="ChatBot Icon" onClick={onClick} />
   );
 };
+
 
 export default ChatBotIcon;
